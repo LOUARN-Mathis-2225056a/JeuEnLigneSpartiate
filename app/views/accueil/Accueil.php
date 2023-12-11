4a9a183fef3cd3341794427d163dfa52ae5a0877
singesinge <thomasbout662@gmail.com>
2023-12-11 18:09:40 +0100

    ajout des bonnes images et link avec le css

--- app/views/accueil/Accueil.php
+++ app/views/accueil/Accueil.php
@@ -1,6 +1,10 @@
 <button class="btn-jouer">JOUER</button>
 <button class="btn-regles">REGLES DU HOCKEY</button>
-<img src="assets/ressources/Homer_Simpson.png">
+<img class="background1" src="/JeuEnLigneSpartiate/public/assets/ressources/background1.jpeg">
+<img class="logo" src="/JeuEnLigneSpartiate/public/assets/ressources/logospartiates.png">
 <p id="spartiates">SPARTIATES</p>
 <p id="hockey">HOCKEY</p>
 <p id="game">GAME</p>
+
+<link rel="stylesheet" href="/JeuEnLigneSpartiate/public/assets/css/Accueil.css">
+<script src="/JeuEnLigneSpartiate/public/assets/js/Accueil.js"></script>
