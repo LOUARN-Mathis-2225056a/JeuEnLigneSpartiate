9831a5c93f2e5decc0de669eb6bc3ba2b725acfb
singesinge <thomasbout662@gmail.com>
2023-12-11 17:08:38 +0100

    debut page accueil

---
+++ app/views/accueil/Accueil.php
@@ -0,0 +1,6 @@
+<button class="btn-jouer">JOUER</button>
+<button class="btn-regles">REGLES DU HOCKEY</button>
+<img src="assets/ressources/Homer_Simpson.png">
+<p id="spartiates">SPARTIATES</p>
+<p id="hockey">HOCKEY</p>
+<p id="game">GAME</p>
