667ba1e3312eb9149c5d678f07d26dbc32fcccd5
singesinge <thomasbout662@gmail.com>
2023-12-12 17:45:46 +0100

    Ajout de tout les éléments et avancement du css

--- app/views/accueil/Accueil.php
+++ app/views/accueil/Accueil.php
@@ -1,10 +1,17 @@
-<button class="btn-jouer">JOUER</button>
-<button class="btn-regles">REGLES DU HOCKEY</button>
-<img class="background1" src="/JeuEnLigneSpartiate/public/assets/ressources/background1.jpeg">
-<img class="logo" src="/JeuEnLigneSpartiate/public/assets/ressources/logospartiates.png">
-<p id="spartiates">SPARTIATES</p>
-<p id="hockey">HOCKEY</p>
-<p id="game">GAME</p>
+<div class="imagefond">
+    <div class="conteneur1">
+        <img class="logo" src="/JeuEnLigneSpartiate/public/assets/ressources/logospartiates.png" alt="logo">
+        <div class="conteneur2">
+            <div class="titre">
+                <h1>SPARTIATES <br> HOCKEY <br> GAME</h1>
+            </div>
+        </div>
+        <div class="conteneur3">
+            <button href="" class="boutton-jouer">JOUER</button>
+            <button href="" class="boutton-regles">REGLES DU JEU</button>
+        </div>
+    </div>
+</div>
 
-<link rel="stylesheet" href="/JeuEnLigneSpartiate/public/assets/css/Accueil.css">
-<script src="/JeuEnLigneSpartiate/public/assets/js/Accueil.js"></script>
+<link rel="stylesheet" href="/JeuEnLigneSpartiate/public/assets/styles/Accueil.css">
+<script src="/JeuEnLigneSpartiate/public/assets/javaScript/Accueil.js"></script>
