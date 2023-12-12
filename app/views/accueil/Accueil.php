1bf8b5271df77bb644545bb2de01a757f720d2d9
singesinge <thomasbout662@gmail.com>
2023-12-12 20:14:54 +0100

    enfin fini yazebi j'ai mis trop de temps ff

--- app/views/accueil/Accueil.php
+++ app/views/accueil/Accueil.php
@@ -1,17 +1,12 @@
-<div class="imagefond">
-    <div class="conteneur1">
-        <img class="logo" src="/JeuEnLigneSpartiate/public/assets/ressources/logospartiates.png" alt="logo">
-        <div class="conteneur2">
-            <div class="titre">
-                <h1>SPARTIATES <br> HOCKEY <br> GAME</h1>
-            </div>
-        </div>
-        <div class="conteneur3">
-            <button href="" class="boutton-jouer">JOUER</button>
-            <button href="" class="boutton-regles">REGLES DU JEU</button>
-        </div>
-    </div>
+<img class="logo" src="/JeuEnLigneSpartiate/public/assets/ressources/logospartiates.png" alt="logo">
+<div class="titre">
+    <div>SPARTIATES</div>
+    <div>HOCKEY</div>
+    <div>GAME</div>
 </div>
+<button href="lien a mettre vers une autre page" class="boutton-jouer">JOUER</button>
+<button href="lien a mettre vers une autre page" class="boutton-regles">REGLES DU JEU</button>
+
 
 <link rel="stylesheet" href="/JeuEnLigneSpartiate/public/assets/styles/Accueil.css">
 <script src="/JeuEnLigneSpartiate/public/assets/javaScript/Accueil.js"></script>
