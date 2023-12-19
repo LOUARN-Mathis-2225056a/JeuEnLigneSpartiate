8a9ef339ac7f20c2ddc971c1a75dd6b724efdfee
GruZbob <alexandre.bergeron@etu.univ-amu.fr>
2023-12-19 19:51:43 +0100

    pages de regles générales et matérielles

---
+++ app/views/regles/ReglesMaterielles.php
@@ -0,0 +1,64 @@
+<header>
+    <button onclick="window.location.href='../accueil/Accueil.php'" class="headerButton">Accueil</button>
+    <button onclick="window.location.href='./ReglesGenerales.php'" class="headerButton">Regles generales</button>
+    <button onclick="window.location.href='./ReglesMaterielles.php'" class="headerButton currentPageButton">Regles materielles</button>
+    <button class="headerButton">Regles de jeu</button>
+    <button class="headerButton">Penalites</button>
+</header>
+
+<nav>
+    <a href="#ancrePalet">Le Palet</a>
+    <a href="#ancreCrosse">La Crosse</a>
+</nav>
+
+<button type="button" aria-label="toggle curtain navigation" class="nav-toggler">
+    <span class="line l1"></span>
+    <span class="line l2"></span>
+    <span class="line l3"></span>
+</button>
+
+<div class="pageContent">
+    <a id="ancrePalet"></a>
+    <div class="contentBloc">
+        <label class="contentTitle">
+            le palet
+        </label>
+        <label class="contentText">
+            Le palet utilisé est <label class="importantWord">noir</label>, il est donc facile à repérer sur une glace blanche
+            <br> <br> <br>
+            il est fait en <label class="importantWord">caoutchouc</label> et pèse entre <label class="importantWord">156 et 170 grammes</label> ce qui lui permet de voler en l'air
+            lorsqu'il est tiré
+            <br> <br> <br>
+            Le palet a la particularité d'être <label class="importantWord">complètement plat</label>, tant sur le dessus que sur le dessous, de sorte qu'il puisse glisser en douceur sur la glace
+        </label>
+    </div>
+    <a id="ancreCrosse"></a>
+    <div class="contentBloc">
+        <label class="contentTitle">
+            la crosse
+        </label>
+        <label class="contentText">
+            La crosse de hockey sur glace est en <label class="importantWord">composite ou en fibre de carbone</label>
+            <br> <br> <br>
+            elle doit avoir des <label class="importantWord">bords courbés</label> et le <label class="importantWord">haut du bâton jusqu'au début de l'extrémité du bâton doit être parfaitement droite</label>
+            <br> <br> <br>
+            Si on fait <label class="importantWord">tomber</label> une crosse pendant le jeu, il est permis de la ramasser mais <label class="importantWord">seulement sa propre crosse</label>
+            <br> <br> <br>
+            La longueur d'une crosse est <label class="importantWord">choisie par le joueur</label> lui-même mais doit être d'une <label class="importantWord">longueur maximale de 163 cm</label>
+            <br> <br> <br>
+            Un joueur <label class="importantWord">offensif</label> préfère généralement une crosse <label class="importantWord">plus courte</label> en raison de l'avantage de pouvoir
+            mieux contrôler le palet, tandis qu'à l'inverse, un <label class="importantWord">défenseur</label> préfèrera une crosse <label class="importantWord">longue</label> pour prendre le palet à un adversaire
+            en position de défense
+            <br> <br> <br>
+            Un paramètre important dans le choix d'une crosse est le <label class="importantWord">flex</label>, qui est un terme désignant le degré de <label class="importantWord">flexibilité et de rigidité</label>
+            dont est dotée la crosse : Un flex élevé équivaut
+            à une crosse rigide, tandis qu'un flex faible vous donne une crosse qui plie beaucoup
+            <br> <br> <br>
+            En pratique, le flex affecte la <label class="importantWord">puissance</label> que vous pouvez obtenir dans votre frappe
+        </label>
+    </div>
+</div>
+
+
+<link rel="stylesheet" href="/JeuEnLigneSpartiate/public/assets/styles/Regles.css">
+<script src="/JeuEnLigneSpartiate/public/assets/javaScript/Regles.js"></script>
