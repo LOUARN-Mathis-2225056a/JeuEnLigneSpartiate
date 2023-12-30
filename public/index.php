91890070d636730f847b4a6635bd84b5572502ff
mat <matthis.louarn@etu.univ-amu.fr>
2023-12-30 14:38:37 +0100

    modification de l'index en conséquence

--- public/index.php
+++ public/index.php
@@ -2,22 +2,28 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 use app\controllers\Login as loginController;
+use app\controllers\PageIntrouvable as pageIntrouvableController;
+use app\controllers\TableauDeBord as tableauDeBordController;
 session_start();
-echo "feur originel";
 try {
-//    if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] === 'POST') {
-//        if (isset($_POST['login'])) {
-//            echo "feur";
-//            (new LoginController())->login($_POST);
-//        }
-//    }
+    if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] === 'POST') {
+        if (isset($_POST['login'])) {
+            echo "feur";
+            (new LoginController())->login($_POST);
+        }
+    }
     if (isset($_SERVER['REQUEST_URI']) && isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] === 'GET') {
         $route = ($_SERVER['REQUEST_URI'] === '/') ? '/' : explode('/', trim($_SERVER['REQUEST_URI'], '/'));
 
         switch ($route[0]){
             case 'login':
-                echo "feur en second";
                 (new loginController())->execute();
+                break;
+            case 'tableau-de-bord':
+                (new tableauDeBordController())->execute();
+                break;
+            default:
+                (new pageIntrouvableController())->execute();
                 break;
         }
     }
