ae9415eee87b35ecd844261dc312576edf2e41e6
AlexandreBergeronb22003371 <alexandrebergeron12@gmail.com>
2024-01-08 15:18:17 +0100

    page règles du jeu

--- app/views/reglesDuJeu/ReglesDuJeu.php
+++ app/views/reglesDuJeu/ReglesDuJeu.php
@@ -10,7 +10,9 @@ class ReglesDuJeu
 
         <label class="titre">LES REGLES DU JEU</label>
 
-        <label>testtest etest tete tet teteteetetetetet</label>
+        <label>Le but est simple ! <br><br> Il s'agit d'un <label class="motImportant">quizz</label> où vous devez cliquer sur la <label class="motImportant">bonne réponse</label> ! <br><br> Une fois que vous avez cliqué,
+            le tireur envoie le palet à toute vitesse dans la réponse choisie.
+            <br><br>Faites un <label class="motImportant">maximum</label> de bonnes réponses pour avoir le <label class="motImportant">meilleur score</label> de toute la <label class="motImportant">patinoire</label> !</label>
 
         <button onclick="window.location.href='../accueil'" class="retour">accueil</button>
 
